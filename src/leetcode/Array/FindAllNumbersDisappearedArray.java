1fb26f8c796b0de1c89f2b9d625547fc951d8c54
idea-PC\lenovo <1052767882@qq.com>
2017-06-22 16:28:16 +0800

    array

---
+++ src/leetcode/Array/FindAllNumbersDisappearedArray.java
@@ -0,0 +1,42 @@
+package leetcode.Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lenovo on 2017/6/22.
+ */
+/*
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+Example:
+
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[5,6]
+ */
+public class FindAllNumbersDisappearedArray {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            int val=nums[i]-1;
+            nums[val]=-nums[val];
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                res.add(i+1);
+            }
+        }
+
+        return res;
+
+    }
+}
